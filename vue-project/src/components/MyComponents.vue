0415f4ea6269594609841c91a6120ab0e4fd4c82
Erik-Kipu <ervinmalyshev@gmail.com>
2025-02-27 21:44:24 +0300

    Задания Vue

---
+++ vue-project/src/components/MyComponents.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>{{ name }}</div> <!-- Выводим имя -->
+    <div>{{ surn }}</div> <!-- Выводим фамилию -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'john',
+      surn: 'smit',
+    };
+  },
+};
+</script>
+
