cd29dc8d0e40b33d160118ad72483a707218c9c6
Erik-Kipu <ervinmalyshev@gmail.com>
2025-03-03 04:53:36 +0300

    Задание 59-63

--- vue-project/src/components/MyComponents.vue
+++ vue-project/src/components/MyComponents.vue
@@ -1,18 +1,57 @@
 <template>
   <div>
-    <div>{{ name }}</div> <!-- Выводим имя -->
-    <div>{{ surn }}</div> <!-- Выводим фамилию -->
+    <!-- Список работников -->
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <Employee :name="user.name" :salary="user.salary" :age="user.age" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Employee from "./Employee.vue";
+
 export default {
+  components: {
+    Employee,
+  },
   data() {
     return {
-      name: 'john',
-      surn: 'smit',
+      users: [
+        { id: 1, name: "name1", salary: 100, age: 30 },
+        { id: 2, name: "name2", salary: 200, age: 40 },
+        { id: 3, name: "name3", salary: 300, age: 50 },
+      ],
     };
   },
 };
 </script>
+
+<style>
+.hidden {
+  display: none;
+}
+.is-valid {
+  color: green;
+}
+.is-disabled {
+  opacity: 0.5;
+}
+.text-red {
+  color: red;
+}
+.bg-gray {
+  background-color: gray;
+}
+.done {
+  text-decoration: line-through;
+}
+.selected {
+  font-weight: bold;
+}
+.active {
+  border: 2px solid blue;
+}
+</style>
 
