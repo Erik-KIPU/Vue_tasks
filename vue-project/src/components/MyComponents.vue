63d8690e28206f09c5f4598f0cb97cade92966a7
Erik-Kipu <ervinmalyshev@gmail.com>
2025-03-03 04:07:30 +0300

    Задание 9

--- vue-project/src/components/MyComponents.vue
+++ vue-project/src/components/MyComponents.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <div>{{ name }}</div> <!-- Выводим имя -->
-    <div>{{ surn }}</div> <!-- Выводим фамилию -->
+    <!-- Кнопка для вызова метода -->
+    <button @click="showCurrentDate">Показать текущую дату</button>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      name: 'john',
-      surn: 'smit',
-    };
+  methods: {
+    // Метод для вывода текущей даты через alert
+    showCurrentDate() {
+      const currentDate = new Date().toLocaleDateString(); // Получаем текущую дату
+      alert(`Текущая дата: ${currentDate}`); // Выводим дату в alert
+    },
   },
 };
 </script>
