c815832a36fa03590d2d48da7e88bbb44a6fe12f
Erik-Kipu <ervinmalyshev@gmail.com>
2025-03-03 05:02:29 +0300

    Задание 65-70

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,70 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <h2>Список работников</h2>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <Employee
+          :name="user.name"
+          :salary="user.salary"
+          :age="user.age"
+          @remove="removeUser(user.id)"
+          @edit="editUser"
+          :onLogName="logName"
+          :onLogNameSalary="logNameSalary"
+        />
+      </li>
+    </ul>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <h3>Добавить работника</h3>
+    <form @submit.prevent="addUser">
+      <input v-model="newUser.name" placeholder="Имя" required />
+      <input v-model="newUser.salary" type="number" placeholder="Зарплата" required />
+      <input v-model="newUser.age" type="number" placeholder="Возраст" required />
+      <button type="submit">Добавить</button>
+    </form>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import Employee from "./Employee.vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+export default {
+  components: {
+    Employee,
+  },
+  data() {
+    return {
+      users: [
+        { id: 1, name: "Андрей", salary: 1000, age: 30 },
+        { id: 2, name: "Ольга", salary: 2000, age: 40 },
+        { id: 3, name: "Виктор", salary: 3000, age: 50 },
+      ],
+      newUser: { name: "", salary: "", age: "" },
+    };
+  },
+  methods: {
+    logName(name) {
+      console.log("Имя работника:", name);
+    },
+    logNameSalary(name, salary) {
+      console.log(`Работник: ${name}, Зарплата: ${salary}`);
+    },
+    removeUser(id) {
+      this.users = this.users.filter(user => user.id !== id);
+    },
+    editUser(updatedUser) {
+      const index = this.users.findIndex(user => user.id === updatedUser.id);
+      if (index !== -1) {
+        this.users.splice(index, 1, updatedUser);
+      }
+    },
+    addUser() {
+      if (this.newUser.name && this.newUser.salary && this.newUser.age) {
+        const id = Date.now();
+        this.users.push({ id, ...this.newUser });
+        this.newUser = { name: "", salary: "", age: "" };
+      }
+    },
+  },
+};
+</script>
---
+++ vue-project/src/components/Employee.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="employee">
+    <p>Имя: <input v-model="editableUser.name" /></p>
+    <p>Зарплата: <input v-model="editableUser.salary" type="number" /></p>
+    <p>Возраст: <input v-model="editableUser.age" type="number" /></p>
+    
+    <button @click="onLogName(editableUser.name)">Вывести имя</button>
+    <button @click="onLogNameSalary(editableUser.name, editableUser.salary)">Вывести имя и зарплату</button>
+    
+    <button @click="$emit('remove')">Удалить</button>
+    <button @click="$emit('edit', editableUser)">Сохранить изменения</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    salary: Number,
+    age: Number,
+    onLogName: Function,
+    onLogNameSalary: Function,
+  },
+  data() {
+    return {
+      editableUser: {
+        name: this.name,
+        salary: this.salary,
+        age: this.age,
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.employee {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 5px 0;
+  border-radius: 5px;
+}
+</style>
+
+
