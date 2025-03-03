d7d438823214c95055b2a523994d10b3ae888f72
Erik-Kipu <ervinmalyshev@gmail.com>
2025-03-03 04:00:14 +0300

    Задание 6

--- vue-project/src/components/MyComponents.vue
+++ vue-project/src/components/MyComponents.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <div>{{ name }}</div> <!-- Выводим имя -->
-    <div>{{ surn }}</div> <!-- Выводим фамилию -->
+    <!-- Задача 1: Квадрат числа num -->
+    <p>Квадрат числа {{ num }}: {{ square }}</p>
+
+    <!-- Задача 2: Сумма чисел num1, num2 и num3 -->
+    <p>Сумма чисел {{ num1 }}, {{ num2 }} и {{ num3 }}: {{ sum }}</p>
   </div>
 </template>
 
@@ -9,10 +12,21 @@
 export default {
   data() {
     return {
-      name: 'john',
-      surn: 'smit',
+      num: 5, // Задача 1
+      num1: 1, // Задача 2
+      num2: 2, // Задача 2
+      num3: 3, // Задача 2
     };
+  },
+  computed: {
+    // Вычисляем квадрат числа num
+    square() {
+      return this.num ** 2;
+    },
+    // Вычисляем сумму чисел num1, num2 и num3
+    sum() {
+      return this.num1 + this.num2 + this.num3;
+    },
   },
 };
 </script>
-
